5e8f44007c06a42b338eb53581b1db47b736d3ad
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-11-21 18:14:19 +0100

    Creation-template-accueil+initiatives

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php
+/*Page accueil */
+
+/* Déclaration header */
+get_header(); ?>
+
+<!-- Corps du body -->
+<div class="container-fluid mt-5">
+    <h2 class="h2 md-mx-5 mt-5 scroll-left">
+    <?php wp_title(); ?>
+
+    </h2>
+
+    <div class="row">
+        <div class=" center col-md-8  col-lg-10 col-xl-8">
+
+        </div>
+    </div>
+
+</div>
+
+
+<?php 
+/* Déclaration footer */
+
+get_footer(); ?>
---
+++ template-initiatives.php
@@ -0,0 +1,17 @@
+<?php 
+/* Template Name: Initiatives
+ */
+
+
+/* Déclaration header */
+get_header(); ?>
+
+
+
+
+
+
+<?php 
+/* Déclaration footer */
+
+get_footer(); ?>
